62365b53227d0006fa112e028a56ce48c32cd7f7
abunyasha12 <43925928+abunyasha12@users.noreply.github.com>
2023-08-08 16:42:33 +0500

    added https to getCoordinates

--- src/getCoordinates.ts
+++ src/getCoordinates.ts
@@ -16,7 +16,7 @@ export async function getCoordinates(cityName: string): Promise<CityCoordinates>
     let APIkey = "63473091e3dbe3b4db55f48ee898d316"
     try {
 
-        let resp = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${APIkey}`).then(response => response.json())
+        let resp = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${APIkey}`).then(response => response.json())
         if (resp) {
             return resp[0]
         }
