ecf894765989b1cf9b5681c0104f6ac66c314b40
abunyasha12 <43925928+abunyasha12@users.noreply.github.com>
2023-08-08 16:32:08 +0500

    changed vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,12 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue_weather/'
+    : '/',
+  transpileDependencies: [
+    'vuetify'
+  ],
   plugins: [
     vue({ 
       template: { transformAssetUrls }
